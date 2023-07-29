f73f12ab2f3dbc211a55a7cc1bbe639b2c943060
unknown <kevin.backstade@gmail.com>
2023-07-29 20:10:49 +0200

    version 1.6 added supporting Minecraft 1.20+

---
+++ v1.6/src/main/java/ImageMod/ImageBuilder/BlockImageCreationData.java
@@ -0,0 +1,22 @@
+package ImageMod.ImageBuilder;
+
+import java.util.function.Consumer;
+
+import ImageMod.WorldTransformer.WorldTransformAction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+
+/* classes */
+public class BlockImageCreationData {
+    public ResizeableImage  image;
+    public ServerLevel world;
+    public BlockPos pos;
+    public Direction xDir;
+    public Direction yDir;
+    public Direction zDir;
+    public int              blockWidth, blockHeight;
+    
+    public Consumer<Exception> 			  onError;
+    public Consumer<WorldTransformAction> onSuccess;
+}
---
+++ v1.6/src/main/java/ImageMod/WorldTransformer/WorldTransformCreationData.java
@@ -0,0 +1,12 @@
+package ImageMod.WorldTransformer;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+
+public class WorldTransformCreationData {
+	public ServerLevel world;
+	public BlockPos pos;
+	public Direction xDir, yDir, zDir;
+	public int			w, h, d;
+}
