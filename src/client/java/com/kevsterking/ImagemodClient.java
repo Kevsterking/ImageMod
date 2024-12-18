6a1ab186c4c653a0ba562b64ee63a16bc55e21fa
Kevin Bäckstäde <kevin.backstade@gmail.com>
2024-12-18 15:03:22 +0100

    Fabric example template added

---
+++ src/client/java/com/kevsterking/ImagemodClient.java
@@ -0,0 +1,10 @@
+package com.kevsterking;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImagemodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
