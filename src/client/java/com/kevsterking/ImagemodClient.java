a1d5be0bc91cf0e9cca9cd35e334d4ee34cce133
Kevin Bäckstäde <kevin.backstade@gmail.com>
2024-12-18 16:55:02 +0100

    Basic command for fabric

--- src/client/java/com/kevsterking/ImagemodClient.java
+++ src/client/java/com/kevsterking/ImagemodClient.java
@@ -1,10 +1,35 @@
 package com.kevsterking;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.text.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ImagemodClient implements ClientModInitializer {
+
+	public static final String MOD_ID = "imagemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		LOGGER.info("Initializing");
+
+		// Register the command
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			LOGGER.info("Registering Commands");
+			dispatcher.register(ClientCommandManager.literal("image").executes(context -> {
+				context.getSource().sendFeedback(Text.literal("Successfully called command"));
+				return 1;
+			}));
+			LOGGER.info("Commands Registered");
+		});
+
+		LOGGER.info("Initialized");
+
 	}
+
 }
